dc4c9bcadbd8aabca21531ba4dffd8e313d1ecae
Marcos Dias da Conceição <ti.marcosdias@gmail.com>
2018-05-17 17:14:21 -0300

    Barcode Scanner

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+              private barcodeScanner: BarcodeScanner) {
 
   }
 
+  scan() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
 }
