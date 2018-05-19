945dc42b7d69b2a4d6f065aa27f015d03317032f
Marcos Dias da Conceição <ti.marcosdias@gmail.com>
2018-05-19 19:01:17 -0300

    Melhorias no QR Scanner

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { AlertController, Content } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import { QRScanner } from '@ionic-native/qr-scanner';
-import { AlertController } from 'ionic-angular';
-import { Platform } from 'ionic-angular';
-import * as Constants from '../../app/constants';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
+
 export class AboutPage {
+
+  @ViewChild(Content) content;
 
   constructor(public navCtrl: NavController,
               private qrScanner: QRScanner,
@@ -21,18 +22,12 @@ export class AboutPage {
     this.scan();
   }
 
-  closeQrScanner() {
-    this.qrScanner.hide().then(()=>{
-      window.document.querySelector('ion-app').classList.remove('transparent');
-    });
+  ionViewWillLeave() {
+    this.qrScanner.destroy();
   }
 
-  ngOnDestroy() {
-    this.closeQrScanner();
-  }  
-
   scan() {
-    // start scanning
+    console.log('Started sacanning...')
     let scanSub = this.qrScanner.scan().subscribe((text: string) => {
 
       let alert = this.alertCtrl.create({
@@ -41,17 +36,11 @@ export class AboutPage {
         buttons: ['OK']
       });
 
-      this.closeQrScanner(); // hide camera preview
+      this.qrScanner.hide(); // hide camera preview
       scanSub.unsubscribe(); // stop scanning
       alert.present();
     });
 
-    this.qrScanner.show().then(()=>{
-      let unregister = this.platform.registerBackButtonAction(()=>{
-          this.closeQrScanner();
-          unregister();
-      });
-      window.document.querySelector('ion-app').classList.add('transparent');
-    });
+    this.qrScanner.show();
   }
 }
